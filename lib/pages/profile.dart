e6e6d6ed88152c6a732aa31d7626ff8d2ee2af59
Hizar62 <hizarsajjad632@gmail.com>
2024-09-15 15:16:50 +0500

    adding & retreving profile imae

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,6 +2,8 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:live/resources/add_data.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:live/services/auth_service.dart';
 import 'package:live/utils.dart';
 
@@ -14,7 +16,48 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   Uint8List? _image;
+  String? _profileImageUrl;
   bool _isSaving = false;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfileImage();
+  }
+
+  void _loadProfileImage() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      User? currentUser =
+          FirebaseAuth.instance.currentUser; // Get current user's UID
+      if (currentUser != null) {
+        String uid = currentUser.uid;
+
+        // Retrieve the image URL from Firestore using UID
+        DocumentSnapshot userProfile = await FirebaseFirestore.instance
+            .collection('userProfile')
+            .doc(uid)
+            .get();
+
+        if (userProfile.exists) {
+          setState(() {
+            _profileImageUrl =
+                userProfile.get('imageLink'); // Retrieve the image URL
+          });
+        }
+      }
+    } catch (e) {
+      print('Error loading profile image: $e');
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   void selectImage() async {
     try {
@@ -30,35 +73,52 @@ class _ProfileState extends State<Profile> {
   void saveProfile() async {
     if (_image == null) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please select an image before saving.')),
+        const SnackBar(
+          content: Text('Please select an image before saving.'),
+          behavior: SnackBarBehavior.floating,
+          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+        ),
       );
       return;
     }
 
     setState(() {
-      _isSaving = true; // Start saving
+      _isSaving = true;
     });
 
     try {
-      print("Initiating profile save...");
-      String result = await StoreData().saveData(file: _image!);
-      print("Profile save result: $result");
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content: Text(result == 'success'
-                ? 'Profile saved successfully!'
-                : 'Failed to save profile: $result')),
-      );
+      String result =
+          await StoreData().saveData(file: _image!); // Save with UID
+      if (result == 'success') {
+        _loadProfileImage(); // Reload profile image after saving
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Profile saved successfully!'),
+            behavior: SnackBarBehavior.floating,
+            margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Failed to save profile: $result'),
+            behavior: SnackBarBehavior.floating,
+            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+          ),
+        );
+      }
     } catch (e) {
       print("Error in saveProfile function: $e");
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-            content: Text('An error occurred while saving the profile: $e')),
+          content: Text('An error occurred while saving the profile: $e'),
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+        ),
       );
     } finally {
       setState(() {
-        _isSaving = false; // End saving
+        _isSaving = false;
       });
     }
   }
@@ -66,55 +126,73 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              _image != null
-                  ? CircleAvatar(
-                      radius: 64,
-                      backgroundImage: MemoryImage(_image!),
-                    )
-                  : const CircleAvatar(
-                      radius: 64,
-                      backgroundImage: NetworkImage(
-                          'https://img.freepik.com/premium-vector/blue-silhouette-person-s-face-against-white-background_754208-70.jpg'),
-                    ),
-              Positioned(
-                bottom: -10,
-                left: 80,
-                child: IconButton(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                _isLoading
+                    ? const CircularProgressIndicator() // Show loader while image is being fetched
+                    : (_image != null
+                        ? CircleAvatar(
+                            radius: 64,
+                            backgroundImage: MemoryImage(_image!),
+                          )
+                        : (_profileImageUrl != null
+                            ? CircleAvatar(
+                                radius: 64,
+                                backgroundImage:
+                                    NetworkImage(_profileImageUrl!),
+                              )
+                            : const CircleAvatar(
+                                radius: 64,
+                                backgroundImage: NetworkImage(
+                                    'https://img.freepik.com/premium-vector/blue-silhouette-person-s-face-against-white-background_754208-70.jpg'),
+                              ))),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
                     onPressed: selectImage,
-                    icon: const Icon(Icons.add_a_photo)),
-              )
-            ],
-          ),
-          const SizedBox(height: 50),
-          ElevatedButton(
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
               style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 218, 20, 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold)),
+                backgroundColor: const Color.fromARGB(255, 218, 20, 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onPressed: _isSaving ? null : saveProfile,
               child: _isSaving
                   ? const CircularProgressIndicator(color: Colors.white)
-                  : const Text('Save')),
-          const SizedBox(height: 20),
-          ElevatedButton(
+                  : const Text('Save'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
               style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 218, 20, 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold)),
+                backgroundColor: const Color.fromARGB(255, 218, 20, 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onPressed: () async {
                 await AuthService().signout(context: context);
               },
-              child: const Text('Logout')),
-        ],
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/resources/add_data.dart
+++ lib/resources/add_data.dart
@@ -1,19 +1,24 @@
 import 'dart:typed_data';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 final FirebaseStorage _storage = FirebaseStorage.instance;
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final FirebaseAuth _auth =
+    FirebaseAuth.instance; // Initialize FirebaseAuth to get UID
 
 class StoreData {
-  Future<String> uploadImageToStorage(String childName, Uint8List file) async {
+  Future<String> uploadImageToStorage(String uid, Uint8List file) async {
     String downloadUrl = '';
     try {
       print("Starting image upload...");
-      Reference ref = _storage.ref().child(childName);
+      Reference ref = _storage
+          .ref()
+          .child('profileImages')
+          .child(uid); // Use UID in storage path
       UploadTask uploadTask = ref.putData(file);
 
-      // Await upload task completion and check for errors
       TaskSnapshot snapshot = await uploadTask;
       downloadUrl = await snapshot.ref.getDownloadURL();
       print("Image uploaded successfully, URL: $downloadUrl");
@@ -27,11 +32,20 @@ class StoreData {
   Future<String> saveData({required Uint8List file}) async {
     String response = "Some Error Occurred";
     try {
+      // Get the current user's UID
+      User? currentUser = _auth.currentUser;
+      if (currentUser == null) throw 'User is not logged in';
+
+      String uid = currentUser.uid;
+
       print("Saving image to Firebase Storage...");
-      String imageUrl = await uploadImageToStorage('profileImage', file);
+      String imageUrl = await uploadImageToStorage(
+          uid, file); // Pass UID to uploadImageToStorage
 
       print("Saving image URL to Firestore...");
-      await _firestore.collection('userProfile').add({'imageLink': imageUrl});
+      await _firestore.collection('userProfile').doc(uid).set({
+        'imageLink': imageUrl,
+      }); // Save URL under user's UID
 
       print("Data saved successfully to Firestore.");
       response = 'success';
