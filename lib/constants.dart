32cf61e0c30ebacdeaab532f5013fd887367c135
Hizar62 <hizarsajjad632@gmail.com>
2024-09-22 09:20:24 +0500

    fixing bugs

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const int appID = 1856407141;
+  static const String appSign =
+      'c6368b80547ba2fd2a8af983ecc0925a2330b471125ac7a8e685543999b6c0aa';
+}
--- lib/pages/live.dart
+++ lib/pages/live.dart
@@ -1,13 +1,51 @@
 import 'dart:math';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:live/constants.dart';
 import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
 
-class LivePage extends StatelessWidget {
+class LivePage extends StatefulWidget {
   final String liveID; // The Live Stream ID entered by the user
   final bool isHost; // Whether the user is the host or not
 
   const LivePage({super.key, required this.liveID, this.isHost = false});
+
+  @override
+  _LivePageState createState() => _LivePageState();
+}
+
+class _LivePageState extends State<LivePage> {
+  String? userName;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsername(); // Fetch username when the widget is initialized
+  }
+
+  Future<void> fetchUsername() async {
+    String? fetchedUsername = await getUsername();
+    setState(() {
+      userName = fetchedUsername;
+    });
+  }
+
+  Future<String?> getUsername() async {
+    User? currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.uid)
+          .get();
+
+      if (userDoc.exists) {
+        return userDoc['username'];
+      }
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +57,23 @@ class LivePage extends StatelessWidget {
         backgroundColor: Colors.redAccent,
       ),
       body: SafeArea(
-        child: ZegoUIKitPrebuiltLiveStreaming(
-          appID:
-              7826576485, // Replace with your actual appID from ZEGOCLOUD Admin Console
-          appSign:
-              'kjdfjdfkn', // Replace with your actual appSign from ZEGOCLOUD Admin Console
-          userID: 'user_id_${Random().nextInt(100)}',
-          userName: 'user_name_${Random().nextInt(100)}',
-          liveID: liveID, // Pass the Live Stream ID entered in the dialog
-          config: isHost
-              ? ZegoUIKitPrebuiltLiveStreamingConfig.host()
-              : ZegoUIKitPrebuiltLiveStreamingConfig.audience(),
-        ),
+        child: userName == null
+            ? const Center(
+                child:
+                    CircularProgressIndicator()) // Show loading indicator until username is loaded
+            : ZegoUIKitPrebuiltLiveStreaming(
+                appID: Constants
+                    .appID, // Replace with your actual appID from ZEGOCLOUD Admin Console
+                appSign: Constants
+                    .appSign, // Replace with your actual appSign from ZEGOCLOUD Admin Console
+                userID: 'user_id_${Random().nextInt(100)}',
+                userName: userName!, // Pass the fetched username
+                liveID: widget
+                    .liveID, // Pass the Live Stream ID entered in the dialog
+                config: widget.isHost
+                    ? ZegoUIKitPrebuiltLiveStreamingConfig.host()
+                    : ZegoUIKitPrebuiltLiveStreamingConfig.audience(),
+              ),
       ),
     );
   }
